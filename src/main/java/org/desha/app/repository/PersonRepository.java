e5447a9239d0cf24ea2427bd4e6e345cfa2d384e
iver62 <iver62@hotmail.fr>
2025-01-14 22:54:48 +0100

    Suppression des classes <Person> repositories et remplacement par une classe générique

--- src/main/java/org/desha/app/repository/PersonRepository.java
+++ src/main/java/org/desha/app/repository/PersonRepository.java
@@ -3,14 +3,13 @@ package org.desha.app.repository;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import org.desha.app.domain.entity.Person;
 
 import java.util.List;
 
 @ApplicationScoped
-public class PersonRepository implements PanacheRepository<Person> {
+public class PersonRepository<T> implements PanacheRepository<T> {
 
-    public Uni<List<Person>> findByIds(List<Long> ids) {
+    public Uni<List<T>> findByIds(List<Long> ids) {
         return list("id IN ?1", ids);
     }
 
